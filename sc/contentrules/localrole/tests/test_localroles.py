4b970eea7721e6dfe6e474f5075245f0fbb1e8c7
Érico Andrei <erico@simplesconsultoria.com.br>
2011-09-06 19:24:18 -0300

    PEP-8 adjustments

--- sc/contentrules/localrole/tests/test_localroles.py
+++ sc/contentrules/localrole/tests/test_localroles.py
@@ -19,11 +19,13 @@ from Products.PloneTestCase.setup import default_user
 
 from DateTime import DateTime
 
+
 class DummyEvent(object):
     implements(IObjectEvent)
-    
+
     def __init__(self, object):
         self.object = object
+
 
 class TestLocalRoleAction(TestCase):
 
@@ -32,56 +34,62 @@ class TestLocalRoleAction(TestCase):
         self.portal.invokeFactory('Folder', 'folder')
         self.folder = self.portal['folder']
 
-    def testRegistered(self): 
-        element = getUtility(IRuleAction, name='sc.contentrules.localrole.ApplyLocalRole')
-        self.assertEquals('sc.contentrules.localrole.ApplyLocalRole', element.addview)
+    def testRegistered(self):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.localrole.ApplyLocalRole')
+        self.assertEquals('sc.contentrules.localrole.ApplyLocalRole',
+                           element.addview)
         self.assertEquals('edit', element.editview)
         self.assertEquals(IObjectManager, element.for_)
         self.assertEquals(IObjectEvent, element.event)
-    
-    def testInvokeAddView(self): 
-        element = getUtility(IRuleAction, name='sc.contentrules.localrole.ApplyLocalRole')
+
+    def testInvokeAddView(self):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.localrole.ApplyLocalRole')
         storage = getUtility(IRuleStorage)
         storage[u'foo'] = Rule()
         rule = self.portal.restrictedTraverse('++rule++foo')
-        
+
         adding = getMultiAdapter((rule, self.portal.REQUEST), name='+action')
-        addview = getMultiAdapter((adding, self.portal.REQUEST), name=element.addview)
-        
-        addview.createAndAdd(data={'principal' : default_user,
-                                    'roles':set(['Reader',])})
-        
+        addview = getMultiAdapter((adding, self.portal.REQUEST),
+                                   name=element.addview)
+
+        addview.createAndAdd(data={'principal': default_user,
+                                    'roles': set(['Reader', ])})
+
         e = rule.actions[0]
         self.failUnless(isinstance(e, LocalRoleAction))
         self.assertEquals(default_user, e.principal)
-        self.assertEquals(set(['Reader',]), e.roles)
-    
-    def testInvokeEditView(self): 
-        element = getUtility(IRuleAction, name='sc.contentrules.localrole.ApplyLocalRole')
+        self.assertEquals(set(['Reader', ]), e.roles)
+
+    def testInvokeEditView(self):
+        element = getUtility(IRuleAction,
+                             name='sc.contentrules.localrole.ApplyLocalRole')
         e = LocalRoleAction()
-        editview = getMultiAdapter((e, self.folder.REQUEST), name=element.editview)
+        editview = getMultiAdapter((e, self.folder.REQUEST),
+                                    name=element.editview)
         self.failUnless(isinstance(editview, LocalRoleEditForm))
 
-    def testExecute(self): 
+    def testExecute(self):
         e = LocalRoleAction()
         e.principal = default_user
-        e.roles = set(['Reader',])
-        
-        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)), IExecutable)
-        self.assertEquals(True, ex())
-        local_roles = self.folder.get_local_roles_for_userid(userid=e.principal)
-        self.failUnless(tuple(e.roles)==local_roles)        
+        e.roles = set(['Reader', ])
 
-    def testExecuteWithError(self): 
-        #TODO
+        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)),
+                              IExecutable)
+        self.assertEquals(True, ex())
+        localroles = self.folder.get_local_roles_for_userid(userid=e.principal)
+        self.failUnless(tuple(e.roles)==localroles)
+
+    def testExecuteWithError(self):
         e = LocalRoleAction()
         e.principal = '%s-non-existent-user' % default_user
-        e.roles = set(['Reader',])
-        
-        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)), IExecutable)
-        self.assertEquals(True, ex())
-        local_roles = self.folder.get_local_roles_for_userid(userid=e.principal)
-        self.failIf(tuple(e.roles)==local_roles)
+        e.roles = set(['Reader', ])
+
+        ex = getMultiAdapter((self.portal, e, DummyEvent(self.folder)),
+                             IExecutable)
+        self.assertEquals(False, ex())
+
 
 def test_suite():
     from unittest import TestSuite, makeSuite
