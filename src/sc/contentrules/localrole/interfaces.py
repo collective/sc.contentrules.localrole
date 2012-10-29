7927470b6f2aaaee0bcb34688447d2deb60c376d
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-29 15:28:26 -0200

    PEP8

--- src/sc/contentrules/localrole/interfaces.py
+++ src/sc/contentrules/localrole/interfaces.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import re
-from zope.interface import implements
 from zope.interface import Interface
-from zope import schema
+
+from zope.schema import Choice
+from zope.schema import Set
+from zope.schema import TextLine
 
 from sc.contentrules.localrole import MessageFactory as _
 
@@ -11,16 +12,15 @@ class ILocalRoleAction(Interface):
     """An action used to apply a local role to an object
     """
 
-    principal = schema.TextLine(title=_(u"Username / Group name"),
-                      description=_(u"Please inform the username or groupname \
-                                      that will be used by this action."),
-                      required=True,
-                )
+    principal = TextLine(title=_(u"Username / Group name"),
+                         description=_(u"Please inform the username or "
+                                       u"groupname that will be used by "
+                                       u"this action."),
+                         required=True)
 
-    roles = schema.Set(title=_(u"Roles"),
-                         description=_(u"Roles to be assigned to user/group \
-                                         in the object."),
-                         required=True,
-                         value_type=schema.Choice(
-                                    vocabulary='plone.app.vocabularies.Roles',
-                ))
+    roles = Set(title=_(u"Roles"),
+                description=_(u"Roles to be assigned to user/group "
+                              u"in the object."),
+                required=True,
+                value_type=Choice(vocabulary='plone.app.vocabularies.Roles')
+                )
