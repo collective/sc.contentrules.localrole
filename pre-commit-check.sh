d2f8853367e51a0965a52ebce9c2d515fd34b0a6
Érico Andrei <erico@simplesconsultoria.com.br>
2012-10-29 15:33:28 -0200

    Fix path

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -15,7 +15,7 @@ echo '====== Running PyFlakes ======'
 bin/zopepy setup.py flakes; handle_exit
 
 echo '====== Running pep8 =========='
-bin/pep8 --ignore=E501 sc/contentrules/localrole; handle_exit
+bin/pep8 --ignore=E501 src/sc/contentrules/localrole; handle_exit
 bin/pep8 --ignore=E501 setup.py; handle_exit
 
 if [ $EXITCODE -ne 0 ]; then
